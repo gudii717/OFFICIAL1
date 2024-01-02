3de7148d94531c8428d8f52408832423c25e669c
gudii717 <150800273+gudii717@users.noreply.github.com>
2024-01-02 20:33:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let dynamo = document.querySelector('body');
+
+let colors = ['linear-gradient(240deg, red, pink)', 'linear-gradient(240deg, teal, aqua)'];
+
+let count = 0;
+
+function gradient() {
+    
+    if (count >= colors.length) {
+        count = 0;
+    }
+    
+    dynamo.style.background = colors[count];
+    
+    count++;
+}
+
+setInterval(gradient, 3000);
